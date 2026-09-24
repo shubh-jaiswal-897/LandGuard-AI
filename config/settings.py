78e69b3d149fe9f446a565d3c2848442be02c480
Shubh Jaiswal <sj7518378908@gmail.com>
2026-09-24 13:21:06 +0530

    Configure settings for Vercel and update admin

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-weo=_0m2_@-uhn_zej=87_t&lf+om(w$=o7ro279qy@&(1yr07'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-weo=_0m2_@-uhn_zej=87_t&lf+om(w$=o7ro279qy@&(1yr07')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Vercel par hum DEBUG=False set karenge
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Project, LandAcquisition
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('project_id', 'project_name', 'state', 'district', 'project_status')
+    search_fields = ('project_id', 'project_name', 'state', 'district')
+    list_filter = ('project_status', 'state')
+
+@admin.register(LandAcquisition)
+class LandAcquisitionAdmin(admin.ModelAdmin):
+    list_display = ('acquisition_id', 'project', 'acquired_area', 'pending_area', 'compensation_pending')
+    search_fields = ('acquisition_id', 'project__project_name')
