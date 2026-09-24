00b2d7922b6dcfcee177ce76d2db9bde3a281969
Shubh Jaiswal <sj7518378908@gmail.com>
2026-09-24 12:50:54 +0530

    Fix PEP 668 internally managed python in Vercel build script

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 echo "Building project..."
 
 # Install dependencies
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
 
 # Run migrations (Optional: better to do it manually if using external DB)
 # python3 manage.py migrate
